789eaf6b0a3f3e2e6f65d35dd6cc532d6e190d9d
Timothy Liu <tlkh@live.co.uk>
2019-01-02 13:08:49 +0800

    initial commit

--- libproxmox.py
+++ libproxmox.py
@@ -1,1 +1,93 @@
+import hashlib
+import subprocess
+import csv
 
+
+class Proxmox(object):
+
+    def __init__(self):
+        self.salt = "iTrst"
+
+    def run_cmd(self, bash_command):
+        print("[INFO  ] Running:", bash_command)
+        with open("stdout.log", "wb") as out, open("stderr.log", "wb") as err:
+            process = subprocess.Popen(
+                bash_command.split(), stdout=out, stderr=err)
+            process.wait()
+            output, error = process.communicate()
+        return output
+
+    def deploy_from_csv(self, file_path):
+        with open(file_path, "rb") as file:
+            csvfile = csv.reader(file)
+            for row in csvfile[1:]:
+                print("[INFO  ]", row)
+                self.deploy_vm(row)
+
+    def deploy_vm(self, row):
+        template_id, target_id, user_displayname = row[0], row[1], row[2]
+        ci_ip_addr, vm_type = row[3], row[4]
+        vm_type = str(vm_type)
+
+        target_name = str(user_displayname).replace(" ", "").replace(",", "")+vm_type
+
+        if "windows" in vm_type.lower():
+            windows_vm = True
+        else:
+            windows_vm = False
+
+        self.clone_vm(template_id, target_id, target_name)
+        self.set_ci(target_id, user_displayname, ci_ip_addr, windows_vm=windows_vm)
+
+    def del_vm(self, target_id):
+        target_id = str(int(target_id))
+        command = "qm destroy "+target_id
+        self.run_cmd(command)
+
+    def start_vm(self, target_id):
+        target_id = str(int(target_id))
+        command = "qm start "+target_id
+        self.run_cmd(command)
+
+    def stop_vm(self, target_id):
+        target_id = str(int(target_id))
+        command = "qm stop "+target_id
+        self.run_cmd(command)
+    
+    def clone_vm(self, template_id, target_id, target_name=None):
+        template_id = str(int(template_id))
+        target_id = str(int(target_id))
+        if target_name is not None:
+            target_name = str(target_name)
+        else:
+            target_name = str(template_id)+"_clone"
+
+        command = "qm clone "+template_id+" "+target_id+" --name "+target_name
+        self.run_cmd(command)
+
+    def set_ci(self, target_vm, display_name, ip_addr, windows_vm=False):
+        target_vm = str(int(target_vm))
+        display_name = str(display_name)
+        ip_addr = str(ip_addr)
+        if len(ip_addr.split(".")) != 4:
+            raise ValueError("[ERROR ] Invalid IP address provided: "+ip_addr)
+
+        if windows_vm:
+            citype = "configdrive2"
+        else:
+            citype = "nocloud"
+
+        username = display_name.lower().replace(" ", "").replace(",", "")
+
+        h = hashlib.new('ripemd160')
+        h.update(bytes(display_name+self.salt))
+        password = str(h.hexdigest())
+
+        commands = []
+        commands.append("qm set "+target_vm+"--citype "+citype)
+        commands.append("qm set "+target_vm+"--ciuser "+username)
+        commands.append("qm set "+target_vm+"--cipassword "+password)
+        commands.append("qm set "+target_vm+"--ipconfig0 ip= "+ip_addr+"/16")
+
+        for command in commands:
+            self.run_cmd(command)
