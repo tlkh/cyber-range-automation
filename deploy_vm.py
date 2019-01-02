c0e3481787ab49e82f9a26d481b9245700ed978f
Timothy Liu <tlkh@live.co.uk>
2019-01-02 17:01:20 +0800

    modified structure

--- deploy_vm.py
+++ deploy_vm.py
@@ -2,7 +2,7 @@ import csv
 
 from libproxmox import Proxmox
 
-proxmox = Proxmox(target_node="svr-corpnet03")
+proxmox = Proxmox()
 
 CSV_DIR = "./deployment.csv"
 print("[INFO  ] Deploying from:", CSV_DIR)
@@ -14,11 +14,46 @@ row_count = 0
 with open(CSV_DIR, "r") as file:
     csvfile = csv.reader(file)
     for row in csvfile:
-        if row_count==0:
+        if row_count == 0:
             pass
         else:
             print("[DEBUG ]:", row)
-            deployed.append(proxmox.deploy_vm(row))
+            template_id, target_id, user_displayname = row[0], row[1], row[2]
+            ci_ip_addr, vm_type = row[3], row[4]
+            vm_type = str(vm_type)
+
+            target_name = str(user_displayname).replace(" ", "").split(",")
+            target_name = target_name[0] + target_name[1].upper()
+            target_name = target_name + "-" + vm_type
+
+            if "windows" in vm_type.lower():
+                windows_vm = True
+            else:
+                windows_vm = False
+
+            self.clone_vm(template_id, target_id, target_name)
+
+            time.sleep(1)
+
+            print("[INFO  ] Cloned:", target_id)
+            deployed.append(target_id)
+
+        time.sleep(1)
+        row_count += 1
+
+row_count = 0
+
+with open(CSV_DIR, "r") as file:
+    csvfile = csv.reader(file)
+    for row in csvfile:
+        if row_count == 0:
+            pass
+        else:
+            print("[DEBUG ]:", row)
+            template_id, target_id, user_displayname = row[0], row[1], row[2]
+            ci_ip_addr, vm_type = row[3], row[4]
+            vm_type = str(vm_type)
+            self.set_ci(target_id, user_displayname,ci_ip_addr, windows_vm=windows_vm)
         time.sleep(1)
         row_count += 1
 
@@ -26,5 +61,6 @@ print("[INFO  ] VMs deployed:", deployed)
 
 for vm_id in deployed:
     proxmox.start_vm(vm_id)
+    time.sleep(1)
 
 print("[INFO  ] VMs started:", deployed)
--- libproxmox.py
+++ libproxmox.py
@@ -19,29 +19,6 @@ class Proxmox(object):
             print("[ERROR ] Error:", str(error))
         return output
 
-    def deploy_vm(self, row):
-        template_id, target_id, user_displayname = row[0], row[1], row[2]
-        ci_ip_addr, vm_type = row[3], row[4]
-        vm_type = str(vm_type)
-
-        target_name = str(user_displayname).replace(" ", "").split(",")
-        target_name = target_name[0] + target_name[1].upper()
-        target_name = target_name + "-" + vm_type
-
-        if "windows" in vm_type.lower():
-            windows_vm = True
-        else:
-            windows_vm = False
-
-        self.clone_vm(template_id, target_id, target_name)
-
-        time.sleep(5)
-
-        self.set_ci(target_id, user_displayname, ci_ip_addr, windows_vm=windows_vm)
-
-        print("[INFO  ] Deployed:", target_id)
-        return target_id
-
     def del_vm(self, target_id):
         target_id = str(int(target_id))
         command = "qm destroy "+target_id
@@ -89,10 +66,11 @@ class Proxmox(object):
         password = str(h.hexdigest())
 
         commands = []
-        commands.append("qm set "+target_vm+"--citype "+citype)
-        commands.append("qm set "+target_vm+"--ciuser "+username)
-        commands.append("qm set "+target_vm+"--cipassword "+password)
-        commands.append("qm set "+target_vm+"--ipconfig0 ip= "+ip_addr+"/16")
+        commands.append("qm set "+target_vm+" --citype "+citype)
+        commands.append("qm set "+target_vm+" --ciuser "+username)
+        commands.append("qm set "+target_vm+" --cipassword "+password)
+        commands.append("qm set "+target_vm+" --ipconfig0 ip= "+ip_addr+"/16")
 
         for command in commands:
             self.run_cmd(command)
+            time.sleep(1)
